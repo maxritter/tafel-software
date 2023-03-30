12aaa53195712dd72cae9be4feaccff8d180d08f
Florian Kneist <fkneist@gmail.com>
2023-03-30 11:30:01 +0200

    Add prettier

--- src/kunden.js
+++ src/kunden.js
@@ -2,29 +2,29 @@
  * Hier liegen alle Methoden zum Zugriff auf
  * die Kundendaten
  */
-"use strict";
+'use strict';
 
-const fs = require("fs");        // Dateizugriffe
-const os = require("os");
+const fs = require('fs'); // Dateizugriffe
+const os = require('os');
 
-var kundenDB = null;             // Die Datenbank enthält alle Daten
-var besucheHeute = {};                // Die Besucher heute
+var kundenDB = null; // Die Datenbank enthält alle Daten
+var besucheHeute = {}; // Die Besucher heute
 var besucheHeuteDate = today();
 var spaltenNamen, spaltenIndex;
 
-// Spalten 
+// Spalten
 var ColGueltig, ColBesuch, ColName, ColErwachsene, ColKinder, ColAB, ColFarbe;
 
-setInterval( () => {
+setInterval(() => {
   if (besucheHeuteDate == today()) return;
   besucheHeuteDate = today();
   besucheHeute = {};
-  }, 60000);
+}, 60000);
 
 // Liefert true wenn die Datenbank bereit ist
-exports.isDatenbankReady = function() {
-    return kundenDB != null;
-}
+exports.isDatenbankReady = function () {
+  return kundenDB != null;
+};
 
 // Stellt die Informationen für einen HTML Link bereit
 class Link {
@@ -39,14 +39,13 @@ class Link {
       let d = g[ColGueltig];
       if (d == undefined) d = '';
       else if (typeof d == 'string') d = d.trim();
-      if (!isDateFormat(d) || d < today())
-        this.expired = true;
+      if (!isDateFormat(d) || d < today()) this.expired = true;
     }
   }
 }
 
 // Lesen der Kundendaten aus einer Datei
-exports.readDatenbank = function(dateiname, spaltenNamenUndIndex) {
+exports.readDatenbank = function (dateiname, spaltenNamenUndIndex) {
   spaltenNamen = spaltenNamenUndIndex[0];
   spaltenIndex = spaltenNamenUndIndex[1];
   ColName = spaltenIndex[0];
@@ -73,43 +72,42 @@ exports.readDatenbank = function(dateiname, spaltenNamenUndIndex) {
 
   if (kundenDB.changed == undefined) kundenDB.changed = [];
   let date = today();
-  Object.keys(kundenDB.aktiv).forEach(key => {
+  Object.keys(kundenDB.aktiv).forEach((key) => {
     let cust = kundenDB.aktiv[key];
-    if (date == cust[ColBesuch])
-      besucheHeute[key] = true;
+    if (date == cust[ColBesuch]) besucheHeute[key] = true;
   });
   return null;
-}
+};
 
 // Schreibt die Datenbank in die Datei zurück
-exports.writeDatenbank = function(dateiname) {
+exports.writeDatenbank = function (dateiname) {
   try {
     fs.writeFileSync(dateiname, JSON.stringify(kundenDB, null, ' '));
   } catch (err) {
     console.error(err);
-    return "Fehler beim Schreiben der Kundendatei";
+    return 'Fehler beim Schreiben der Kundendatei';
   }
-}
+};
 
-exports.resetChanged = function() {
+exports.resetChanged = function () {
   kundenDB.changed = [];
-}
+};
 
 // Liefert Tabell mit allen Kunden
-exports.getKundenAsTable = function() {
+exports.getKundenAsTable = function () {
   let table = [];
   getAsTable(kundenDB.aktiv, table);
   let lastAktive = table.length;
   getAsTable(kundenDB.inaktiv, table);
-  for(let ix = 0; ix < table.length; ix++) {
-    table[ix].push(ix < lastAktive ? "A" : "G");
+  for (let ix = 0; ix < table.length; ix++) {
+    table[ix].push(ix < lastAktive ? 'A' : 'G');
   }
   return table;
-}
+};
 
 function getAsTable(hashMap, result) {
   // Alle Kunden anhängen
-  Object.keys(hashMap).forEach(id => {
+  Object.keys(hashMap).forEach((id) => {
     result.push(splitAdresse(prepareValues(id, hashMap[id])));
   });
 }
@@ -117,15 +115,17 @@ function getAsTable(hashMap, result) {
 // Ersetzt die Adresse im Array durch Straße, PLZ und Ort
 function splitAdresse(arr) {
   let s = arr[2];
-  let v1 = "", v2 = "", v3 = "";
+  let v1 = '',
+    v2 = '',
+    v3 = '';
   if (typeof s == 'string') {
     let ix = s.indexOf(',');
     if (ix >= 0) {
       v1 = s.substring(0, ix).trim(); // Straße
-      v3 = s.substring(ix + 1).trim();  // Ort oder PLZ Ort
+      v3 = s.substring(ix + 1).trim(); // Ort oder PLZ Ort
       if (v3.search(/[0-9]{5}/) == 0) {
-        v2 = v3.substring(0, 5); // PLZ 
-        v3 = v3.substring(5).trim();  // Nur der Ort
+        v2 = v3.substring(0, 5); // PLZ
+        v3 = v3.substring(5).trim(); // Nur der Ort
       }
     } else {
       v1 = s; // Keine PLZ und kein Ort
@@ -136,54 +136,60 @@ function splitAdresse(arr) {
 }
 
 // Liefert einen Kunden
-exports.getKunde = function(id) {
+exports.getKunde = function (id) {
   let kunde = kundenDB.aktiv[id];
-  if (kunde == undefined) return {message: id};
-  let result = {visited: besucheHeute[id] != undefined, message: ''};
-  if (result.visited) result.message = "Ein Kundenbesuch wurde heute bereits registriert!";
+  if (kunde == undefined) return { message: id };
+  let result = { visited: besucheHeute[id] != undefined, message: '' };
+  if (result.visited)
+    result.message = 'Ein Kundenbesuch wurde heute bereits registriert!';
   else {
     if (typeof kunde[ColGueltig] != 'string') {
-      result.message = "Kein Gültig bis eingetragen!";
+      result.message = 'Kein Gültig bis eingetragen!';
     } else {
       kunde[ColGueltig] = kunde[ColGueltig].trim();
       if (!isDateFormat(kunde[ColGueltig])) {
-        result.message = "Bitte beachten: " + kunde[ColGueltig] + "!";
+        result.message = 'Bitte beachten: ' + kunde[ColGueltig] + '!';
       } else {
         if (kunde[ColGueltig] < today()) {
-          result.message = "Die Berechtigung ist abgelaufen!";
+          result.message = 'Die Berechtigung ist abgelaufen!';
           result.dateInvalid = true;
         } else {
-          let days = (new Date(kunde[ColGueltig]) - new Date(today()))/ 86400000;
+          let days =
+            (new Date(kunde[ColGueltig]) - new Date(today())) / 86400000;
           if (days < 21) {
             let text;
-            if (days == 0) text = "heute";
-            else if (days == 1) text = "morgen";
-            else text = "in " + days + " Tagen";
-            result.message = "Die Berechtiging läuft " + text + " ab!";
+            if (days == 0) text = 'heute';
+            else if (days == 1) text = 'morgen';
+            else text = 'in ' + days + ' Tagen';
+            result.message = 'Die Berechtiging läuft ' + text + ' ab!';
             result.dateWarn = true;
           }
         }
       }
     }
   }
-  result.labels = ["ID"].concat(spaltenNamen);
+  result.labels = ['ID'].concat(spaltenNamen);
   result.bean = prepareValues(id, kunde);
   return result;
-}
+};
 
 // Liefert einen neuen Kuden
-exports.getKundeNeu = function(kunde, cnf) {
-  let result = {visited: false, message: '', labels: ["ID"].concat(spaltenNamen)};
-  result.bean = [ '' ].concat(kunde);
-  for (;result.bean.length <= spaltenNamen.length;) result.bean.push("");
+exports.getKundeNeu = function (kunde, cnf) {
+  let result = {
+    visited: false,
+    message: '',
+    labels: ['ID'].concat(spaltenNamen),
+  };
+  result.bean = [''].concat(kunde);
+  for (; result.bean.length <= spaltenNamen.length; ) result.bean.push('');
 
   let ab = {};
-  Object.keys(kundenDB.aktiv).forEach(id => {
+  Object.keys(kundenDB.aktiv).forEach((id) => {
     let kunde = kundenDB.aktiv[id];
     let v = kunde[ColAB];
     let f = kunde[ColFarbe];
-    if (typeof v == 'string' && typeof f == 'string')  {
-      let key = v + " " + f;
+    if (typeof v == 'string' && typeof f == 'string') {
+      let key = v + ' ' + f;
       if (ab[key] == undefined) ab[key] = 1;
       else ab[key]++;
     }
@@ -191,7 +197,7 @@ exports.getKundeNeu = function(kunde, cnf) {
   result.bean[0] = newId(cnf);
   result.abUndFarbe = ab;
   return result;
-}
+};
 
 // Liefert eine neue ID im Format jjmmddnn0 (Jahr Monat Tag Nummer)
 // Wenn für den aktuellen Tag keine ID frei ist, dann wird es mit dem
@@ -202,51 +208,55 @@ function newId(cnf) {
 function newIdInt(ms, cnf) {
   let ids = [];
   let date = todayCompact(cnf.vollesjahr, ms);
-  Object.keys(kundenDB.aktiv).forEach(id => {
+  Object.keys(kundenDB.aktiv).forEach((id) => {
     if (id.startsWith(date)) ids.push(id);
-  })
-  Object.keys(kundenDB.inaktiv).forEach(id => {
+  });
+  Object.keys(kundenDB.inaktiv).forEach((id) => {
     if (id.startsWith(date)) ids.push(id);
-  })
+  });
   if (ids.length == 0) {
-    date = date + ["1", "01", "001", "0001"][cnf.tag - 1];
-    if (cnf.unternummer) date = date + "0";
+    date = date + ['1', '01', '001', '0001'][cnf.tag - 1];
+    if (cnf.unternummer) date = date + '0';
     return date;
   }
   ids = ids.sort();
-  let maxid = ids[ids.length -1];
+  let maxid = ids[ids.length - 1];
   if (cnf.unternummer) maxid = maxid.substring(0, maxid.length - 1);
   //console.log(maxid);
-  let newid = parseInt("1" + maxid);
+  let newid = parseInt('1' + maxid);
   newid++;
-  newid = ("" + newid).substring(1);
-  if (cnf.unternummer) newid = newid + "0";
+  newid = ('' + newid).substring(1);
+  if (cnf.unternummer) newid = newid + '0';
   if (newid.startsWith(date)) return newid;
   return newIdInt(ms + 86400000, cnf); // Einen Tag weiterschalten
 }
 function todayCompact(longyear, ms) {
   let date = new Date(ms).toISOString();
-  return date.substring((longyear ? 0 : 2) , 4) + date.substring(5,7) + date.substring(8,10);
+  return (
+    date.substring(longyear ? 0 : 2, 4) +
+    date.substring(5, 7) +
+    date.substring(8, 10)
+  );
 }
 
 // Liefert einen inaktiven Kunden
-exports.getKundeInaktiv = function(id) {
+exports.getKundeInaktiv = function (id) {
   let kunde = kundenDB.inaktiv[id];
-  if (kunde == undefined) return {message: id};
-  let result = {visited: false, inaktiv: true};
-  result.labels = ["ID"].concat(spaltenNamen);
+  if (kunde == undefined) return { message: id };
+  let result = { visited: false, inaktiv: true };
+  result.labels = ['ID'].concat(spaltenNamen);
   result.bean = prepareValues(id, kunde);
   return result;
-}
+};
 
 // Ändert einen Kunden
-exports.change = function(id, bean) {
+exports.change = function (id, bean) {
   if (id == undefined || id != bean[0]) {
     // Neuanlage oder Ändeung der ID
     if (kundenDB.aktiv[bean[0]] != undefined)
-      return "Die ID " + bean[0] + " existiert bereits!";
+      return 'Die ID ' + bean[0] + ' existiert bereits!';
     if (kundenDB.inaktiv[bean[0]] != undefined)
-      return "Die ID " + bean[0] + " existiert bereits und ist gelöscht!";
+      return 'Die ID ' + bean[0] + ' existiert bereits und ist gelöscht!';
   }
   let kunde = [];
   for (let i = 0; i < spaltenIndex.length; i++) {
@@ -265,7 +275,7 @@ exports.change = function(id, bean) {
   if (today() == kunde[ColBesuch]) besucheHeute[bean[0]] = true;
   else delete besucheHeute[bean[0]];
   return null;
-}
+};
 
 // Die Änderungen ermitteln und bei Neuerfassung oder
 // geänderten Attributen für den Druck die ID in die
@@ -284,17 +294,19 @@ function determineChanges(idNew, idOld, kundeNew) {
   }
   let kundeOld = kundenDB.aktiv[idOld];
   if (kundeOld == undefined) kundeOld = kundenDB.inaktiv[idOld];
-  
+
   if (kundeOld == undefined) {
     // sollte nicht sein - als geändert vermerken
     addChanged(idNew);
     return;
   }
-  if (kundeNew[ColName] == kundeOld[ColName] &&
-      kundeNew[ColErwachsene] == kundeOld[ColErwachsene] &&
-      kundeNew[ColKinder] == kundeOld[ColKinder] &&
-      kundeNew[ColAB] == kundeOld[ColAB] &&
-      kundeNew[ColFarbe] == kundeOld[ColFarbe]) {
+  if (
+    kundeNew[ColName] == kundeOld[ColName] &&
+    kundeNew[ColErwachsene] == kundeOld[ColErwachsene] &&
+    kundeNew[ColKinder] == kundeOld[ColKinder] &&
+    kundeNew[ColAB] == kundeOld[ColAB] &&
+    kundeNew[ColFarbe] == kundeOld[ColFarbe]
+  ) {
     // sind gleich
     return;
   }
@@ -314,7 +326,7 @@ function removeChanged(id) {
 }
 
 // Einen Kunden löschen
-exports.delete = function(id) {
+exports.delete = function (id) {
   removeChanged(id);
   let kunde = kundenDB.aktiv[id];
   if (kunde == undefined) {
@@ -325,23 +337,24 @@ exports.delete = function(id) {
     delete besucheHeute[id];
   }
   return null;
-}
+};
 
 // Setzt das Datum für den letzten Besuch auf heute
-exports.setVisited = function(id, log, SEP) {
+exports.setVisited = function (id, log, SEP) {
   let kunde = kundenDB.aktiv[id];
-  if (kunde == undefined) return {message: "Unbekannte ID: " + id};
+  if (kunde == undefined) return { message: 'Unbekannte ID: ' + id };
   kunde[ColBesuch] = today();
   besucheHeute[id] = true;
-  if (log.file == undefined || log.file == "") return;
+  if (log.file == undefined || log.file == '') return;
   try {
     let data = '"' + new Date().toLocaleString('de-DE') + '"';
     let value;
-    log.columns.forEach(column => {
-      if (column == "ID") value = id;
+    log.columns.forEach((column) => {
+      if (column == 'ID') value = id;
       else value = kunde[column];
       if (value == undefined) value = '';
-      if (typeof value === 'number' || value.match(/[0-9]+/)) data += SEP + value;
+      if (typeof value === 'number' || value.match(/[0-9]+/))
+        data += SEP + value;
       else {
         value = value.replace(/"/g, '""');
         value = value.replace(/\n/g, '\\n');
@@ -353,33 +366,33 @@ exports.setVisited = function(id, log, SEP) {
     return null;
   } catch (err) {
     console.error(err);
-    return "Fehler beim Schreiben der Logdatei";
+    return 'Fehler beim Schreiben der Logdatei';
   }
-}
+};
 
 // Setzt neues Gültig bis
-exports.setNewDate = function(id, date) {
+exports.setNewDate = function (id, date) {
   let kunde = kundenDB.aktiv[id];
-  if (kunde == undefined) return "ID existiert nicht mehr: " + id;
+  if (kunde == undefined) return 'ID existiert nicht mehr: ' + id;
   kunde[ColGueltig] = new Date(date).toISOString().substring(0, 10);
   return null;
-}
+};
 
 // Liefert alle IDs
-exports.getIds = function() {
+exports.getIds = function () {
   return macheLinks(Object.keys(kundenDB.aktiv), false);
-}
+};
 
 // Liefert alle IDs
-exports.getIdsChanged = function() {
+exports.getIdsChanged = function () {
   return macheLinks(kundenDB.changed, false);
-}
+};
 
 // Erzeugt die Links aus einer List von IDs
 function macheLinks(liste, mitName) {
   let result = [];
   let name;
-  liste.forEach(key => {
+  liste.forEach((key) => {
     if (mitName) name = kundenDB.aktiv[key][ColName];
     else name = key;
     result.push(new Link(key, name, true));
@@ -391,7 +404,7 @@ function macheLinks(liste, mitName) {
 function getInaktive() {
   let result = [];
   let name;
-  Object.keys(kundenDB.inaktiv).forEach(key => {
+  Object.keys(kundenDB.inaktiv).forEach((key) => {
     name = kundenDB.inaktiv[key][ColName];
     result.push(new Link(key, name, false));
   });
@@ -402,7 +415,7 @@ function getInaktive() {
 function getAbsent(weeks) {
   let result = [];
   let name;
-  Object.keys(kundenDB.aktiv).forEach(key => {
+  Object.keys(kundenDB.aktiv).forEach((key) => {
     let absent = false;
     let d = kundenDB.aktiv[key][ColBesuch];
     if (d == undefined) d = '';
@@ -410,7 +423,7 @@ function getAbsent(weeks) {
     if (!isDateFormat(d)) {
       absent = true;
     } else {
-      let days = (new Date(today()) - new Date(d))/ 86400000;
+      let days = (new Date(today()) - new Date(d)) / 86400000;
       if (days > weeks * 7) {
         absent = true;
       }
@@ -424,28 +437,31 @@ function getAbsent(weeks) {
 }
 
 // Liefert die Links für die Besucher heute, die gelöschten oder einen Buchstaben
-exports.getName = function(letter, option) {
+exports.getName = function (letter, option) {
   if (letter == 'scanned') return macheLinks(Object.keys(besucheHeute), true);
-  if (letter == "Inaktiv") return getInaktive();
-  if (letter == "Absent") return getAbsent(option);
+  if (letter == 'Inaktiv') return getInaktive();
+  if (letter == 'Absent') return getAbsent(option);
 
   var include = includeLetter;
   if (letter == 'rest') include = includeRest;
-  else if (letter == 'name') include = function() { return true };
+  else if (letter == 'name')
+    include = function () {
+      return true;
+    };
   let result = [];
-  Object.keys(kundenDB.aktiv).forEach(key => {
+  Object.keys(kundenDB.aktiv).forEach((key) => {
     let name = kundenDB.aktiv[key][ColName];
     if (name == null) name = ' ';
     if (include(name.substring(0, 1).toUpperCase(), letter))
       result.push(new Link(key, name, true));
   });
   return sortLinks(result);
-}
+};
 
 // Liefert die Links für die aktuelle erwarteten Kunden
-exports.getNameFor = function(checks) {
+exports.getNameFor = function (checks) {
   let result = [];
-  Object.keys(kundenDB.aktiv).forEach(key => {
+  Object.keys(kundenDB.aktiv).forEach((key) => {
     let kunde = kundenDB.aktiv[key];
     if (includeCheck(kunde, checks)) {
       let name = kunde[ColName];
@@ -453,11 +469,11 @@ exports.getNameFor = function(checks) {
     }
   });
   return sortLinks(result);
-}
+};
 
 // Sortiert die Links nach dem Wert
 function sortLinks(array) {
-  array.sort( (a, b) => {
+  array.sort((a, b) => {
     if (a.value > b.value) return 1;
     if (a.value < b.value) return -1;
     return 0;
@@ -467,11 +483,10 @@ function sortLinks(array) {
 
 function includeCheck(kunde, checks) {
   for (let i = 0; i < checks.length; i++) {
-    if (kunde[checks[i][0]] != checks[i][1])
-      return false;
+    if (kunde[checks[i][0]] != checks[i][1]) return false;
   }
   return true;
-} 
+}
 
 function includeLetter(chara, letter) {
   if (chara == letter) return true;
@@ -490,18 +505,22 @@ function prepareValues(id, inarray) {
   var array = [id];
   for (let i = 0; i < spaltenIndex.length; i++) {
     let index = spaltenIndex[i];
-    if (inarray[index] == undefined) array.push("");
-    else  array.push(inarray[index]);
+    if (inarray[index] == undefined) array.push('');
+    else array.push(inarray[index]);
   }
   return array;
 }
 
 // Liefert die Statistik für den letzten Öffnungstag
-exports.statistik = function(schreibeFile, SEP) {
+exports.statistik = function (schreibeFile, SEP) {
   let ids = Object.keys(kundenDB.aktiv);
-  let datum = "0000-00-00";
-  let besucher = 0, besucherE = 0, besucherK = 0, erwachsene = 0, kinder = 0;
-  ids.forEach(id => {
+  let datum = '0000-00-00';
+  let besucher = 0,
+    besucherE = 0,
+    besucherK = 0,
+    erwachsene = 0,
+    kinder = 0;
+  ids.forEach((id) => {
     let kunde = kundenDB.aktiv[id];
     let letzterbesuch = kunde[ColBesuch];
     let e = parseInt(kunde[ColErwachsene]);
@@ -524,22 +543,42 @@ exports.statistik = function(schreibeFile, SEP) {
     }
   });
   if (schreibeFile === true) {
-    let csv = '"' + datum + '"' + SEP +
-              ids.length + SEP + erwachsene + SEP + kinder + SEP +
-              besucher + SEP + besucherE + SEP + besucherK + os.EOL;
-    let fn = datum + "-statistik.csv";
+    let csv =
+      '"' +
+      datum +
+      '"' +
+      SEP +
+      ids.length +
+      SEP +
+      erwachsene +
+      SEP +
+      kinder +
+      SEP +
+      besucher +
+      SEP +
+      besucherE +
+      SEP +
+      besucherK +
+      os.EOL;
+    let fn = datum + '-statistik.csv';
     try {
       fs.writeFileSync(fn, csv);
     } catch (err) {
       console.error(err);
-      console.log("Fehler beim Schreiben der Statistikdatei " + fn);
+      console.log('Fehler beim Schreiben der Statistikdatei ' + fn);
     }
     return null;
   }
-  return { datum: datum,
-            berechtigte: ids.length, erwachsene: erwachsene, kinder: kinder,
-            besucher: besucher, besucherE: besucherE, besucherK: besucherK};
-}
+  return {
+    datum: datum,
+    berechtigte: ids.length,
+    erwachsene: erwachsene,
+    kinder: kinder,
+    besucher: besucher,
+    besucherE: besucherE,
+    besucherK: besucherK,
+  };
+};
 
 function isDateFormat(datestring) {
   return datestring == datestring.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/);
